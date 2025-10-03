38eda338d427afaa47220108fa9a6598d5e0f246
rhafi <excyuef@gmail.com>
2025-10-03 09:55:01 +0700

    rgrg

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,14 @@
 //const { document } = require("postcss");
 const nav = document.querySelector('#nav');
 const menu = document.querySelector('#hamburger');
+const exit = document.querySelector('#');
 
-menu.addEventListener('click', (e) => {
-  e.preventDefault();
-  nav.classList.add('active');
-});
+menu.addEventListener("click", () => {
+  nav.classList.remove("hilang")
+  nav.classList.add("muncul")
+})
+
+exit.addEventListener("click", () => {
+  menu.classList.remove("muncul")
+  menu.classList.add("ilang")
+})
