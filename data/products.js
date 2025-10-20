098e961b6aad7cfa4544ed91d20a682f82ebb7b2
rhafi <excyuef@gmail.com>
2025-10-20 13:12:57 +0700

    cs

---
+++ data/products.js
@@ -0,0 +1,23 @@
+let cart  = [{
+  nama:"kopi susu",
+  id: "id1",
+  quantity: 0,
+},
+  
+  {
+  nama:"matcha",
+  id: "id2",
+  quantity: 0,
+},
+
+  {
+  nama:"americano",
+  id: "id3",
+  quantity: 0,
+},
+
+  {
+  nama:"kopi susu gula aren",
+  id: "id4",
+  quantity: 0,
+}];
